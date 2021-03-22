bebafdd9b7d4b54b8a602c339f7778c17c30f1f1
Sergio López <sergioloc2@gmail.com>
2021-03-22 13:23:34 +0100

    Change layout design

--- app/src/main/java/com/slc/klock/MainActivity.kt
+++ app/src/main/java/com/slc/klock/MainActivity.kt
@@ -5,7 +5,6 @@ import android.app.TimePickerDialog
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import java.sql.Time
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
@@ -29,14 +28,15 @@ class MainActivity : AppCompatActivity() {
         val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
             hour1 = hour
             min1 = minute
-            if (minute == 0)
-                tvTime1.text = "$hour:${minute}0h"
-            else
-                tvTime1.text = "$hour:${minute}h"
+            when {
+                minute == 0 -> tvTime1.text = "$hour:${minute}0"
+                minute < 10 -> tvTime1.text = "$hour:0${minute}"
+                else -> tvTime1.text = "$hour:${minute}"
+            }
             if (hour1 != -1 && hour2 != -1 && min1 != -1 && min2 != -1)
                 setOperation()
         }
-        TimePickerDialog(this, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
+        TimePickerDialog(this, R.style.PinkDialog, timeSetListener, cal.get(Calendar.HOUR_OF_DAY), cal.get(Calendar.MINUTE), true).show()
     }
 
     @SuppressLint("SetTextI18n")
@@ -45,10 +45,11 @@ class MainActivity : AppCompatActivity() {
         val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
             hour2 = hour
             min2 = minute
-            if (minute == 0)
-                tvTime2.text = "$hour:${minute}0h"
-            else
-                tvTime2.text = "$hour:${minute}h"
+            when {
+                minute == 0 -> tvTime1.text = "$hour:${minute}0"
+                minute < 10 -> tvTime1.text = "$hour:0${minute}"
+                else -> tvTime2.text = "$hour:${minute}"
+            }
             if (hour1 != -1 && hour2 != -1 && min1 != -1 && min2 != -1)
                 setOperation()
         }
@@ -58,6 +59,7 @@ class MainActivity : AppCompatActivity() {
     @SuppressLint("SetTextI18n")
     private fun setOperation(){
         when {
+            hour1 == hour2 && min1 == min2 -> tvCount.text = resources.getString(R.string.default_time)
             hour1 > hour2 -> tvCount.text = calculateDifference(hour1, min1, hour2, min2)
             hour1 < hour2 -> tvCount.text = "- " + calculateDifference(hour2, min2, hour1, min1)
             min1 > min2 -> tvCount.text = calculateDifference(hour1, min1, hour2, min2)
@@ -66,10 +68,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun calculateDifference(highHour: Int, hightMinute: Int, lowHour: Int, lowMinute: Int): String{
-        var hourDiff = -1
-        var minDiff = -1
-
-        hourDiff = highHour - lowHour
+        var hourDiff: Int = highHour - lowHour
+        val minDiff: Int
 
         if (hightMinute >= lowMinute)
             minDiff = hightMinute - lowMinute
